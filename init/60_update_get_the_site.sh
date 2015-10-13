73809ca4c92a517e26e633b1000fff258a6ed98b
sparklyballs <sparklyballs@gmail.com>
2015-10-13 16:30:28 +0100

    possible checkstyle fixes

--- init/60_update_get_the_site.sh
+++ init/60_update_get_the_site.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+THUMBS="/Thumbs"
+PICTURES="/Pictures"
+
 if [ ! -f "/config/www/PhotoShow/config.php" ]; then
 echo "fetching photoshow files from github"
 git clone https://github.com/thibaud-rohmer/PhotoShow.git /config/www/PhotoShow
-sed -i -e 's/$config->photos_dir.\+/$config->photos_dir = "\/Pictures";/' /config/www/PhotoShow/config.php
-sed -i -e 's/$config->ps_generated.\+/$config->ps_generated = "\/Thumbs";/' /config/www/PhotoShow/config.php
+sed -i -e "s#$config->photos_dir.\+#$config->photos_dir = \"${PICTURES}\";#" /config/www/PhotoShow/config.php
+sed -i -e "s#$config->ps_generated.\+#$config->ps_generated = \"${THUMBS}\";#" /config/www/PhotoShow/config.php
 else
 echo "checking for updates"
 cd /config/www/PhotoShow
