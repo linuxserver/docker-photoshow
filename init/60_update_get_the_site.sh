184615f6b25d13f01ad4608a88437f618b9a74ca
sparklyballs <sparklyballs@gmail.com>
2016-01-18 14:06:59 +0000

    add opt out for autoupdates to git update init

--- init/60_update_get_the_site.sh
+++ init/60_update_get_the_site.sh
@@ -8,14 +8,16 @@ echo "fetching photoshow files from github"
 git clone https://github.com/thibaud-rohmer/PhotoShow.git /config/www/PhotoShow
 sed -i -e "s#$config->photos_dir.\+#$config->photos_dir = \"${PICTURES}\";#" /config/www/PhotoShow/config.php
 sed -i -e "s#$config->ps_generated.\+#$config->ps_generated = \"${THUMBS}\";#" /config/www/PhotoShow/config.php
-else
-echo "checking for updates"
-cd /config/www/PhotoShow
-git pull
 fi
 
 # patch in fixed keyboard.js file
 cp /defaults/keyboard.js /config/www//PhotoShow/src/js/keyboard.js
-
 sed -i -e "s@\$config->timezone.*@\$config->timezone = \"${TZ}\"@g" /config/www/PhotoShow/config.php
 chown -R abc:abc /config/www/PhotoShow /Thumbs
+
+# opt out for autoupdates
+[ "$ADVANCED_DISABLEUPDATES" ] && exit 0
+
+cd /config/www/PhotoShow
+git pull
+chown -R abc:abc /config/www/PhotoShow
